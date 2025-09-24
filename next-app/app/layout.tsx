72f9d5875cbe1e79420c81bb7375e6cadfe3a125
kudeta-japan <yuya.is.yuya@gmail.com>
2025-09-24 17:48:38 +0900

    Create layout.tsx

---
+++ next-app/app/layout.tsx
@@ -0,0 +1,11 @@
+import './globals.css';
+
+export const metadata = { title: 'Meltidippot' };
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
